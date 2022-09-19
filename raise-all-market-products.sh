4bea123986be97d244eb5333b33500975de85449
Jacek <jacek.lajdecki@axonivy.com>
2022-09-19 15:59:09 +0200

    refactor: separate tasks into functions

--- raise-all-market-products.sh
+++ raise-all-market-products.sh
@@ -3,22 +3,52 @@
 # Usage: raise-all-market-products.sh <version>
 #
 
-ignoredRepos="market|market-product|demo-projects|Apache License 2.0"
-workDir=$(mktemp -d -t projectConvertXXX)
+ignoredRepos=(
+  "market"
+  "market-product"
+  "demo-projects"
+  "Apache License 2.0"
+)
+
+if [ -z "$workDir" ]; then
+  workDir=$(mktemp -d -t projectConvertXXX)
+fi
 
 convert_to_version=$1
 if [ -z "$convert_to_version" ]; then
-  read -p "Please enter a version you want to convert to: " convert_to_version
+  echo "Missing target version parameter e.g 9.4.0"
+  exit 1
 fi
 
-curl https://api.github.com/orgs/axonivy-market/repos | 
-grep -e '"name"' | 
-sed -e 's/"name": "//' -e 's/",//' |
-while read line
-do
-    if [[ $line =~ $ignoredRepos ]]; then
-        continue
-    fi
-    echo "Migrating $line"
-    source ./repo-migrator.sh $convert_to_version $line
-done
+
+collectRepos() {
+  curl https://api.github.com/orgs/axonivy-market/repos | 
+  grep -e '"name"' | 
+  sed -e 's/"name": "//' \
+      -e 's/",//'
+}
+
+migrateListOfRepos() {
+  collectRepos |
+  while read repo; do
+    migrateRepo $repo
+  done
+}
+
+migrateRepo() {
+  repo=$1
+  if [[ $repo =~ $ignoredRepos ]]; then
+    echo "Skipping repository $repo"
+  else
+    echo "Migrating $repo to $convert_to_version"
+    source ./repo-migrator.sh $convert_to_version $repo
+  fi
+}
+
+repoName=$2
+if [ -z "$repoName" ]; then
+  migrateListOfRepos
+else
+  migrateRepo $repoName
+fi
+
